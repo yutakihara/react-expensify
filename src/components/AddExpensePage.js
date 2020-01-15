8a90888cf5ef7130165a66d8a3046bbb4de3f0a1
kiharayuta <k.kyuta.821@gmail.com>
2020-01-14 23:55:12 -0800

    Adding new componenet

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
     <div>
-        <p>This is from my add expense page</p>
+        <h1>Add Expense</h1>
+        <ExpenseForm />
     </div>
 );
 
